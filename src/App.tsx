57b3901e891797a9b95b260a4459f5510165a598
Scott Richards <up777205@myport.ac.uk>
2022-11-02 21:01:54 +0000

    Added score. Added logic for making guess.
    Added logic for setting random option to be correct.

--- src/App.tsx
+++ src/App.tsx
@@ -20,19 +20,42 @@ function decimalToHex(decimalNumber: number) {
   return `${hexChars[bigNumberIndex]}${hexChars[smallNumberIndex]}`;
 }
 
+const firstColor = getRandomHexColor();
+const firstOption = getRandomNumber(3);
+
 function App() {
-  const [color, setColor] = useState(getRandomHexColor());
-  const [colorOption1, setColorOption1] = useState(getRandomHexColor());
-  const [colorOption2, setColorOption2] = useState(getRandomHexColor());
-  const [colorOption3, setColorOption3] = useState(getRandomHexColor());
+  const [score, setScore] = useState(0);
+  const [color, setColor] = useState(firstColor);
+  const [colorOption1, setColorOption1] = useState(
+    firstOption === 0 ? firstColor : getRandomHexColor()
+  );
+  const [colorOption2, setColorOption2] = useState(
+    firstOption === 1 ? firstColor : getRandomHexColor()
+  );
+  const [colorOption3, setColorOption3] = useState(
+    firstOption === 2 ? firstColor : getRandomHexColor()
+  );
+
+  function makeGuess(guess: string) {
+    if (color === guess) {
+      setScore((s) => s + 1);
+    }
+    const nextColor = getRandomHexColor();
+    const nextOption = getRandomNumber(3);
+    setColor(nextColor);
+    setColorOption1(nextOption === 0 ? nextColor : getRandomHexColor());
+    setColorOption2(nextOption === 1 ? nextColor : getRandomHexColor());
+    setColorOption3(nextOption === 2 ? nextColor : getRandomHexColor());
+  }
 
   return (
     <>
+      <span>Score: {score}</span>
       <div className="w-[200px] h-[200px]" style={{ backgroundColor: color }} />
       <div className="flex gap-4 p-4">
-        <Button>{colorOption1}</Button>
-        <Button>{colorOption2}</Button>
-        <Button>{colorOption3}</Button>
+        <Button onClick={() => makeGuess(colorOption1)}>{colorOption1}</Button>
+        <Button onClick={() => makeGuess(colorOption2)}>{colorOption2}</Button>
+        <Button onClick={() => makeGuess(colorOption3)}>{colorOption3}</Button>
       </div>
     </>
   );
